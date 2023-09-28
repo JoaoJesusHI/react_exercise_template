877fe1c55969e4a9198a310d24c179e16df4fb73
Joao Jesus <HCC-JJesus@MBPLC.com>
2023-09-28 13:20:12 +0100

    Added comment

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -2,6 +2,11 @@ import { List } from 'semantic-ui-react'
 
 export default function UserInfo({ user }){
 
+    // Render item user property of type string as using the following item 
+    // <List.Item>
+    //     <List.Header>{_}</List.Header>{user[_]}
+    // </List.Item>
+
     return(
         <List>
         </List>
